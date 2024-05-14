1591a508f53d788c49ff30d451c0a15d9e334d4d
ANNU12344 <annu000060@gmail.com>
2024-05-14 21:45:20 +0530

    updated

--- Wix Service/src/wix_server.py
+++ Wix Service/src/wix_server.py
@@ -7,6 +7,7 @@ from src.api.Routes.update_routes import update_ns
 from src.api.Routes.collection_routes import collection_ns 
 from src.api.Routes.search_routes import search_ns 
 from src.api.Routes.token_routes import token_ns
+from src.api.Routes.post_routes import post_ns
 from src.Domain.Constant.constant import SQLALCHEMY_DATABASE_URI
 from flask_restx import Api
 
@@ -20,9 +21,10 @@ app.register_blueprint(wix_token_bp, url_prefix='/wix_token')
 api = Api(app)
 api.add_namespace(order_ns) #getting the order info by using the order if
 api.add_namespace(product_ns) #getting the product by id
-api.add_namespace(update_ns) 
+api.add_namespace(update_ns) #update the product
 api.add_namespace(collection_ns) #getting the collection information
-api.add_namespace(search_ns )
-api.add_namespace(token_ns)
+api.add_namespace(search_ns ) #search product availablity
+api.add_namespace(token_ns) #update the token
+api.add_namespace(post_ns) #get the post 
 
 

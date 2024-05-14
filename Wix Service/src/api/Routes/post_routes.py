62baec590e8a8511b5a35edeb4799d119f8582c4
ANNU12344 <annu000060@gmail.com>
2024-05-14 21:10:31 +0530

    updated

--- Wix Service/src/api/Routes/post_routes.py
+++ Wix Service/src/api/Routes/post_routes.py
@@ -4,7 +4,7 @@ from src.Interactor.Logger.custom_logger import app_logger
 from src.Interactor.DTR.post_dtr import post_rest_response
 import traceback
 
-post_ns = Namespace('wix_token', description='create the collection')
+post_ns = Namespace('wix_token', description='Get the posts')
 
 post_parser = reqparse.RequestParser()
 post_parser.add_argument('Authorization', type=str, location='headers', help='Authorization header', required=True)

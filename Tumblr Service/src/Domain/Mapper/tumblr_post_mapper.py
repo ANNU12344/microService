af546261fc653ad642de8707b066e0bb15db03a0
ANNU12344 <annu000060@gmail.com>
2024-05-27 11:04:32 +0530

    updated

--- Tumblr Service/src/Domain/Mapper/tumblr_post_mapper.py
+++
@@ -1,9 +0,0 @@
-# from automapper import mapper
-# from src.Domain.Entities.category import PersonalBlogging
-# from src.Interactor.Exception.custom_exceptions import MappingException
-
-# def map_json_to_shop(json_data):
-#     try:
-#         return mapper.to(PersonalBlogging).map(json_data)
-#     except Exception as e:
-#         raise MappingException(e)

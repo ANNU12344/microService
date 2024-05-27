bd108e970e31df105886f9d510e35a7b1a47cf59
ANNU12344 <90202112+ANNU12344@users.noreply.github.com>
2024-05-27 11:03:25 +0530

    Delete Tumblr Service directory

--- Tumblr Service/app.py
+++
@@ -1,7 +0,0 @@
-from src.tumblr_server import app
-def run_flask_server():
-    print("Running Flask server")
-    app.run(debug = True)
-if __name__ == "__main__":
-    run_flask_server()
-
--- Tumblr Service/src/Infra/Repositiories/post_repository.py
+++
@@ -1,9 +0,0 @@
-# from src.Interactor.Database.sql_alchemy import sqlalchemy_db as db
-# from src.api.Models.post import TumblrPost
-
-# class TumblrPostRepository:
-#     def create_tumblr_post(self, data):
-#         new_post = TumblrPost(**data)
-#         db.session.add(new_post)
-#         db.session.commit()
-#         return new_post
